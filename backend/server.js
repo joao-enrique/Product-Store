9d4163be376210ee8b414db0e477a3c46f26e0be
Jedev1 <almeidadesousajoaoenrique@gmail.com>
2025-09-14 12:52:25 -0300

    💻 chore: initialize project with package.json and essential dependencies

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+console.log(`Server is starting on port ${PORT}`);
+
+
+app.use(express.json()); //Express.json é um middleware embutido no Express.js que analisa requisições com payload JSON e transforma o corpo da requisição em um objeto JavaScript acessível via req.body.
+app.use(cors()); //CORS (Cross-Origin Resource Sharing) é um mecanismo que permite que recursos restritos em uma página web sejam solicitados a partir de outro domínio fora do domínio do qual o recurso foi servido.
+app.use(helmet()); //Helmet é uma biblioteca para Express.js que agrega 12 middlewares simples, responsáveis por setar alguns headers nas respostas HTTP.
+app.use(morgan('dev')); //Morgan é um middleware de logging para aplicações Node.js. Ele registra detalhes sobre as requisições HTTP recebidas pelo servidor, como método, URL, status de resposta e tempo de resposta.
+
+app.get('/test', (req, res) => {
+    console.log(res.getHeaders());
+  res.send('Hello backend! test route');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
